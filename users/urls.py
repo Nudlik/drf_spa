757b33ed4a3dd18e2cf33990987ac9c47adaf6cb
Nudlik <gegorp@mail.ru>
2024-03-09 20:57:01 +0700

    ендпоинты для авторизации по JWT

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users import apps
 from users.views import UserViewSet
@@ -10,5 +11,8 @@ router = routers.DefaultRouter()
 router.register(r'', UserViewSet, basename='users')
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
