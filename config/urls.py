aca9aa9314787f5c3bbddbbbd3aeeef681efd200
Nudlik <gegorp@mail.ru>
2024-03-10 05:53:58 +0700

    обновил зависимости, подключил свагер

--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from config.yasg import schema_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('habits/', include('habits.urls')),
+
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
